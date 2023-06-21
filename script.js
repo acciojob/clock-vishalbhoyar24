eb6f094fdae9700bf7fad429b9bf5a61e217b240
vishalbhoyar24 <124866977+vishalbhoyar24@users.noreply.github.com>
2023-06-21 11:48:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let p = document.querySelector("#timer");
+setTimeout(function getTime(){
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hours = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let meridiam = "AM";
+    if(hours >= 12){
+        meridiam = "PM"
+    }else{
+        meridiam = "AM";
+    }
+    p.textContent = `${day}/${month}/${year}, ${hours}:${min}:${sec} ${meridiam}`
+    setTimeout(getTime, 1000);
+}, 1000);
